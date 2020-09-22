87dcb428eeda213d6af5594250648046c71eb135
Ye Xiao <xiaoye.xy127@gmail.com>
2020-09-22 00:37:30 -0700

    initial lab3

---
+++ lab3/Arithmetic/Arithmetic.java
@@ -0,0 +1,14 @@
+package lab3.Arithmetic;
+public class Arithmetic {
+    /** Computes product of two ints */
+
+    public static int product(int a, int b) {
+        return a * b;
+    }
+
+    /** Computes sum of two ints (incorrectly) */
+
+    public static int sum(int a, int b) {
+        return a * b;
+    }   
+}
---
+++ lab3/Flik/Flik.java
@@ -0,0 +1,9 @@
+/**
+ * An Integer tester created by Flik Enterprises.
+ */
+package lab3.Flik;
+public class Flik {
+    public static boolean isSameNumber(Integer a, Integer b) {
+        return a == b;
+    }
+}
