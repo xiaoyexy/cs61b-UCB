1f57a12dbf961f2576b14a736873fbe6d9da2335
Ye Xiao <xiaoye.xy127@gmail.com>
2020-10-05 00:15:19 -0700

    finish proj1gold

---
+++ proj1gold/AssertEqualsStringDemo.java
@@ -0,0 +1,27 @@
+/**
+ * Demos the version of assertEquals with a String message.
+ */
+package proj1gold;
+
+import edu.princeton.cs.introcs.StdRandom;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AssertEqualsStringDemo {
+    @Test
+    public void test1() {
+        int expected = 5;
+        int actual = StdRandom.uniform(9);
+        assertEquals("Oh noooo!\nThis is bad:\n   Random number " + actual
+                        + " not equal to " + expected + "!",
+                expected, actual);
+    }
+
+    /**
+     * This main method is optional.
+     */
+    public static void main(String[] args) {
+        jh61b.junit.TestRunner.runTests(AssertEqualsStringDemo.class);
+    }
+} 
---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,59 @@
+package proj1gold;
+
+import edu.princeton.cs.introcs.StdRandom;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestArrayDequeGold {
+    private StudentArrayDeque<Integer> stuDque = new StudentArrayDeque<>();
+    private ArrayDequeSolution<Integer> solDeque = new ArrayDequeSolution<>();
+
+    @Test
+    public void testOnDeque() {
+        int number, oper;
+
+        Integer actual = 0;
+        Integer expect = 0;
+        String errMsg = "";
+
+        StdRandom.setSeed(43);
+
+        while (actual.equals(expect)) {
+
+            number = StdRandom.uniform(1, 1000);
+            oper = StdRandom.uniform(0, 4);
+
+            switch (oper) {
+                case 0 :
+                    stuDque.addFirst(number);
+                    solDeque.addFirst(number);
+                    errMsg += "addFirst(" + number +")\n";
+                    break;
+                case 1 :
+                    stuDque.addLast(number);
+                    solDeque.addLast(number);
+                    errMsg += "addLast(" + number +")\n";
+                    break;
+                case 2 :
+                    if (stuDque.size() == 0 || solDeque.size() == 0) {
+                        break;
+                    }
+                    expect = stuDque.removeFirst();
+                    actual = solDeque.removeFirst();
+                    errMsg += "removeFirst()\n";
+                    break;
+                case 3 :
+                    if (stuDque.size() == 0 || solDeque.size() == 0) {
+                        break;
+                    }
+                    expect = stuDque.removeLast();
+                    actual = solDeque.removeLast();
+                    errMsg += "removeLast()\n";
+                    break;
+            }
+        }
+
+        assertEquals(errMsg, expect, actual);
+    }
+}
